c5bf6420b5f68ade4275f9bc07d147d921d8a0c4
Noe Duran <durann031@gmail.com>
2018-03-03 13:45:13 -0500

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,103 @@
+const botSettings = require("./botsettings.json");
+const Discord = require("discord.js");
+const prefix = botSettings.prefix
+
+const bot = new Discord.Client({disableEveryone: true});
+
+bot.on("ready", async () => {
+ console.log(`Bot is Ready! ${bot.user.username}`);
+
+ try {
+  let link = await bot.generateInvite(["ADMINISTRATOR"]);
+  console.log(link);
+ } catch(e) {
+  console.log(e.stack);
+ }
+
+});
+
+bot.on("message", async message => {
+	if(message.author.bot) return;
+	if(message.channel.type === "dm") return;
+
+	let messageArray = message.content.split(" ");
+	let command = messageArray[0]
+	let args = messageArray.slice(1);
+	
+	if(!command.startsWith(botSettings.prefix)) return;
+
+	if(command === `${prefix}userinfo`) {
+		let embed = new Discord.RichEmbed()
+			.setAuthor(message.author.username)	
+			.setDescription("This is the user's info!")
+			.setColor("#9B5986")
+			.addField("Full Username", `${message.author.username}#${message.author.discriminator}`)
+			.addField("ID", message.author.id)
+			.addField("Created At", message.author.createdAt);
+
+
+		message.channel.sendEmbed(embed);
+
+		return;
+	}
+
+	if(command === `${prefix}mute`) {
+
+		if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.sendMessage("You dont have manage message");
+
+
+		let toMute = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
+		if(!toMute) return message.channel.sendMessage("You did not specify a user mention or ID!");
+
+		if(toMute.id === message.author.id) return message.channel.sendMessage("You cannot mute yourself");
+		if(toMute.highestRole.position >= message.member.highestRole.position) return message.channel.sendMessage("You can't mute that bitch")
+
+
+		let role = message.guild.roles.find(r => r.name === "RevoBot Muted");
+		if(!role){
+			try{
+				role = await message.guild.createRole({
+					name: "RevoBot Muted",
+					color: "#000000",
+					permissions: []
+				});
+
+				message.guild.channels.forEach(async (channel, id) => {
+					await channel.overwritePermissions(role, {
+						SEND_MESSAGES: false,
+						ADD_REACTIONS: false
+					});
+				});
+			} catch(e){
+				console.log(e.stack);
+			}
+		}
+
+		if(toMute.roles.has(role.id)) return message.channel.sendMessage("This user is already muted!");
+
+		await toMute.addRole(role);
+		message.channel.sendMessage("I have muted them.");
+
+		return;
+	}
+
+	if(command === `${prefix}unmute`) {
+
+		if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.sendMessage("You dont have manage message");
+
+
+		let toMute = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
+		if(!toMute) return message.channel.sendMessage("You did not specify a user mention or ID!");
+
+		let role = message.guild.roles.find(r => r.name === "RevoBot Muted");
+		
+		if(!role || !toMute.roles.has(role.id)) return message.channel.sendMessage("This user is not muted!");
+
+		await toMute.removeRole(role);
+		message.channel.sendMessage("I have unmuted them.");
+
+		return;
+	}
+});
+
+bot.login(botSettings.token);
